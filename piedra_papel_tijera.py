318396e9c7bfea79b516d2ff0bab03415b263685
mayerlyuribe <mayerlyuribe@coleguioguanenta.edu.co>
2023-03-15 10:15:58 -0500

    aún no termino el programa :(

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,26 @@
+import random
+
+
+print("----------------------------------------")
+print("-------- piedra papel tijera -----------")
+print("----------------------------------------")
+
+#input
+
+#processing
+import ramdon
+
+
+Print("----------------------------------------")
+print("----------lanzamiento de un dado--------")
+Print("----------------------------------------")
+
+#input
+jugador=int(input"cual es tu opción?)
+
+if ==1:
+    rta= "PIEDRA"
+elif ==2:
+    rta= "PAPEL"
+elif ==3:
+    rta= "TIJERA"
