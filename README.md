bbb90ffb96f8d129dd69fca3c4eee629494f142d
mayerlyuribe <mayerlyuribe@coleguioguanenta.edu.co>
2023-03-23 14:59:45 -0500

    se corrigió el código y el diagrama de flujo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # piedra_papel_tijera
-programa para simular el juego piedra papel o tijera
+
+# programa para simular el juego piedra papel o tijera
+
+# Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,26 +1,58 @@
+
+# Piedra, papel o tijera
+
 import random
 
+print("--------------------------------------------")
+print("-----------Piedra, papel o tijera-----------")
+print("--------------------------------------------")
 
-print("----------------------------------------")
-print("-------- piedra papel tijera -----------")
-print("----------------------------------------")
+print("1 Corresponde a piedra")
+print("2 Corresponde a papel")
+print("3 Corresponde a tijera")
 
-#input
+# input
 
-#processing
-import ramdon
+j = int(input("¿Qué escoges?: "))
+    
+# processing
+
+m = random.randint(1,3)
+
+if (j == 1):
+    if (m == 1 ):
+        print("Empate")
+        print("Elegiste piedra y la máquina sacó piedra")
+    elif (m==2):
+        print("Lo siento, perdiste")
+        print("Elegiste piedra y la máquina sacó papel")
+    else:
+        print("Felicidades, ganaste")
+        print("Elegiste piedra y la máquina sacó tijera")
+
+        
+if (j == 2):
+    if (m == 1):
+        print("Felicidades, ganaste")
+        print("Elegiste papel y la máquina sacó piedra")
+    elif (m == 2):
+        print("Empate")
+        print("Elegiste papel y la máquina sacó papel")
+    else:
+       print ("Lo siento, perdiste")
+       print("Elegiste papel y la máquina sacó tijera")
 
 
-Print("----------------------------------------")
-print("----------lanzamiento de un dado--------")
-Print("----------------------------------------")
+if (j == 3):
+    if ( m == 1):
+        print("Lo siento, perdiste")
+        print("Elegiste tijera y la máquina sacó piedra")
+    elif ( m ==2):
+        print("Felicidades, ganaste")
+        print("Elegiste tijera y la máquina sacó piedra")
+    else:
+        print("Empate")
+        print("Elegiste tijera y la máquina sacó tijera")
 
-#input
-jugador=int(input"cual es tu opción?)
-
-if ==1:
-    rta= "PIEDRA"
-elif ==2:
-    rta= "PAPEL"
-elif ==3:
-    rta= "TIJERA"
+if (j > 3):
+    print("Ingresaste una opcion inválida, revisa las opciones")
